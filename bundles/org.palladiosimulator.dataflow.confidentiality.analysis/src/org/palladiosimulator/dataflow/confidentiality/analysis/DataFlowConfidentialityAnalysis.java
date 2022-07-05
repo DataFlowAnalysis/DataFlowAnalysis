b399c0298bc4595920825b236537025e19c57d18
Sebastian Hahner <hahner@kit.edu>
2022-07-05 17:04:35 +0200

    Add interface for data flow analysis.

---
+++ bundles/org.palladiosimulator.dataflow.confidentiality.analysis/src/org/palladiosimulator/dataflow/confidentiality/analysis/DataFlowConfidentialityAnalysis.java
@@ -0,0 +1,17 @@
+package org.palladiosimulator.dataflow.confidentiality.analysis;
+
+import java.util.List;
+import org.palladiosimulator.dataflow.confidentiality.analysis.sequence.entity.ActionSequence;
+
+public interface DataFlowConfidentialityAnalysis {
+
+    public boolean initalizeAnalysis();
+
+    public boolean loadModels();
+
+    // only for testing purposes
+    public List<ActionSequence> findAllSequences();
+    
+    // TODO: Add more (real) signatures
+
+}

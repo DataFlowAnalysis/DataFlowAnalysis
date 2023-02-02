773bf77294e196f2d3a2e6a7ca2db70bc4741cf7
Felix Schwickerath <felix.schwickerath@t-online.de>
2023-02-02 12:21:56 +0100

    Add saving for scaliblity results

--- tests/org.palladiosimulator.dataflow.confidentiality.analysis.tests/src/org/palladiosimulator/dataflow/confidentiality/scalability/TestRunner.java
+++ tests/org.palladiosimulator.dataflow.confidentiality.analysis.tests/src/org/palladiosimulator/dataflow/confidentiality/scalability/TestRunner.java
@@ -1,5 +1,9 @@
 package org.palladiosimulator.dataflow.confidentiality.scalability;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -10,9 +14,11 @@ public class TestRunner {
 	private final Logger logger = Logger.getLogger(TestRunner.class);
 	
 	private List<ScalibilityTest> tests;
+	private List<ScalibilityParameter> results;
 	
 	public TestRunner(List<ScalibilityTest> tests) {
 		this.tests = tests;
+		this.results = new ArrayList<>();
 	}
 	
 	public void runTests() {
@@ -25,11 +31,27 @@ public class TestRunner {
 			logger.info("Running test with model size " + modelSize);
 			ScalibilityParameter parameter = new ScalibilityParameter(modelSize);
 			test.run(parameter);
+			this.results.add(parameter);
+			saveResults();
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
+		}
+	}
+	
+	private void saveResults() {
+		try {
+			FileOutputStream fileOutputStream
+		      = new FileOutputStream("results.txt");
+		    ObjectOutputStream objectOutputStream 
+		      = new ObjectOutputStream(fileOutputStream);
+		    objectOutputStream.writeObject(this.results);
+		    objectOutputStream.flush();
+		    objectOutputStream.close();
+		} catch(IOException e) {
+			e.printStackTrace();
 		}
 	}
 }

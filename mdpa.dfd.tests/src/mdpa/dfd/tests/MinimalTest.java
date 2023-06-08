2c356570b748047d84f0a90221bea42b526ef264
Anne Hermann <useju@student.kit.edu>
2023-06-08 18:07:03 +0200

    created minimal model instance, created entry point for behaviour diagram, wrote load and store test

--- mdpa.dfd.tests/src/mdpa/dfd/tests/MinimalTest.java
+++ mdpa.dfd.tests/src/mdpa/dfd/tests/MinimalTest.java
@@ -1,6 +1,14 @@
 package mdpa.dfd.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.util.Collections;
+
+import mdpa.dfd.diagram.dataflowdiagrammodel.DataFlowDiagram;
+import mdpa.dfd.diagram.dataflowdiagrammodel.DataFlowDiagramModelPackage;
+import mdpa.dfd.diagram.dataflowdiagrammodel.impl.DataFlowDiagramModelFactoryImpl;
+
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
@@ -8,38 +16,45 @@ import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 
 import org.junit.jupiter.api.Test;
 
-
 class MinimalTest {
 
-	private static final String RELATIVE_PATH_TO_MINIMAL_EXAMPLE = "?";
-	private DataFlowDiagram dfd; //imports nach generierung noch hinzufügen
+	private static final String RELATIVE_PATH_TO_MINIMAL_EXAMPLE = "resources/MinimalModel.dataflowdiagrammodel";
+	private DataFlowDiagram dfd;
 
 	@Test
-	void loadInstance() {
+	void loadAndSave() {
+		dfd = loadInstance(); //TODO: soll überprüft werden, dass es auch genau das richtige Modell ist?
+		dfd = changeInstance(dfd);
+		saveChangedInstance(dfd);
+	}
+
+	private DataFlowDiagram loadInstance() {
 		ResourceSet resourceSet = new ResourceSetImpl();
 		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap()
-				.put(Resource.Factory.Registry.DEFAULT_EXTENSION, new dataflowdaigrammodelResourceFactoryImpl());
-		resourceSet.getPackageRegistry().put(dataflowdiagrammodelPackage.eNS_URI,
-				dataflowdiagrammodelPackage.eINSTANCE);
+				.put(Resource.Factory.Registry.DEFAULT_EXTENSION, new DataFlowDiagramModelFactoryImpl());
+		resourceSet.getPackageRegistry().put(DataFlowDiagramModelPackage.eNS_URI,
+				DataFlowDiagramModelPackage.eINSTANCE);
 
 		File file = new File(RELATIVE_PATH_TO_MINIMAL_EXAMPLE);
 		URI uri = file.isFile() ? URI.createFileURI(file.getAbsolutePath())
 				: URI.createURI(RELATIVE_PATH_TO_MINIMAL_EXAMPLE);
-
+		
 		Resource resource = resourceSet.getResource(uri, true);
-		dfd = (DataFlowDiagram) resource.getContents().get(0);
+		return (DataFlowDiagram) resource.getContents().get(0);
 
 	}
-	
-	@Test
-	void changeInstance() {
-		
+
+	private DataFlowDiagram changeInstance(DataFlowDiagram dfd) {
+		dfd.getNodes().get(0).setEntityName("NewName");
+		return dfd;
 	}
-	
-	@Test
-	void saveChangedInstance() {
-		
+
+	private void saveChangedInstance(DataFlowDiagram dfd) {
+		ResourceSet resourceSet = new ResourceSetImpl();
+		Resource resource = resourceSet.createResource(URI.createFileURI("path/to/your/model.fileextension"));
+
+		resource.getContents().add(dfd);
+		assertDoesNotThrow(() -> resource.save(Collections.EMPTY_MAP));
 	}
-	
 
 }

f43de2ab3cf7f3720e5992b011d469dd4a1757a1
Felix Schwickerath <felix.schwickerath@student.kit.edu>
2024-01-03 15:59:32 +0100

    test(pcm): add test for unspported seff nodes

--- tests/org.dataflowanalysis.analysis.tests/src/org/dataflowanalysis/analysis/tests/constraint/ConstraintFeatureTest.java
+++ tests/org.dataflowanalysis.analysis.tests/src/org/dataflowanalysis/analysis/tests/constraint/ConstraintFeatureTest.java
@@ -9,6 +9,7 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.dataflowanalysis.analysis.DataFlowConfidentialityAnalysis;
 import org.dataflowanalysis.analysis.core.ActionSequence;
+import org.dataflowanalysis.analysis.core.pcm.user.CallingUserActionSequenceElement;
 import org.dataflowanalysis.analysis.core.pcm.user.UserActionSequenceElement;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -68,5 +69,31 @@ public class ConstraintFeatureTest extends ConstraintTest {
         });
         printViolation(results);
         assertTrue(results.isEmpty());
+    }
+    
+    /**
+     * Test determining whether node characteristics work correctly
+     */
+    @Test
+    @DisplayName("Test whether unknown actions will not cause incorrect results")
+    public void testUnkownSEFFActions() {
+    	var usageModelPath = Paths.get("models", "IgnoredNodeTest", "default.usagemodel");
+    	var allocationPath = Paths.get("models", "IgnoredNodeTest", "default.allocation");
+    	var nodeCharacteristicsPath = Paths.get("models", "IgnoredNodeTest", "default.nodecharacteristics");
+    	DataFlowConfidentialityAnalysis analysis = super.initializeAnalysis(usageModelPath, allocationPath, nodeCharacteristicsPath);
+    	
+    	List<ActionSequence> sequences = analysis.findAllSequences();
+    	List<ActionSequence> propagatedSequences = analysis.evaluateDataFlows(sequences);
+    	
+    	logger.setLevel(Level.TRACE);
+    	var results = analysis.queryDataFlow(propagatedSequences.get(0), node -> {
+    		printNodeInformation(node);
+    		if (node instanceof CallingUserActionSequenceElement && ((CallingUserActionSequenceElement) node).isReturning()) {
+    			return node.getAllDataFlowVariables().size() != 0;
+    		}
+    		return false;
+        });
+        printViolation(results);
+        assertTrue(results.size() == 1);
     }
 }

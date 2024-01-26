6af401a5697574ae0ad1a6a62c4cd5fe463fa4ee
uuqjz <nils.niehues@kit.edu>
2024-01-26 21:27:48 +0100

    Changed node to child

--- bundles/org.dataflowanalysis.analysis.converter/src/org/dataflowanalysis/dfd2json/Main.java
+++ bundles/org.dataflowanalysis.analysis.converter/src/org/dataflowanalysis/dfd2json/Main.java
@@ -5,8 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.ArrayList;
 
-import org.dataflowanalysis.dfd2json.dfd.DFD;
+import org.dataflowanalysis.dfd2json.dfd.*;
 import org.dataflowanalysis.json2dfd.microsecend.SystemConfiguration;
 
 
@@ -59,6 +60,20 @@ public class Main {
         objectMapper = new ObjectMapper();
         try {
         	DFD dfd = objectMapper.readValue(new File("minimal.json"), DFD.class);
+        	List<Child> children =dfd.model().children();
+        	List<Child> nodes = new ArrayList<>();
+        	List<Child> edges = new ArrayList<>();
+        	for (Child child : children) {
+        		if(child.type().contains("node")) {
+        			nodes.add(child);
+        		}
+        		else if(child.type().contains("edge")) {
+        			edges.add(child);
+        		}
+        	}
+        	System.out.println(nodes);
+        	System.out.println(edges);
+        	
         }
         catch (IOException e) {
             System.err.println("Error parsing minimal: ");
--- bundles/org.dataflowanalysis.analysis.converter/src/org/dataflowanalysis/dfd2json/dfd/Node.java
+++
@@ -1,5 +0,0 @@
-package org.dataflowanalysis.dfd2json.dfd;
-
-import java.util.List;
-
-public record Node(String text, List<LabelType> labels, List<Port> ports, String id, String type, String sourceId, String targetId, List<Object> children) {}

c50e8b36feb1dd5c2f40b9841ee9b82ac8f916ff
uuqjz <nils.niehues@kit.edu>
2024-01-26 18:12:14 +0100

    Removed unused record

--- bundles/org.dataflowanalysis.analysis.converter/src/org/dataflowanalysis/dfd2json/dfd/Edge.java
+++
@@ -1,5 +0,0 @@
-package org.dataflowanalysis.dfd2json.dfd;
-
-import java.util.List;
-
-public record Edge(String id, String type, String sourceId, String targetId, String text, List<Object> children) {}

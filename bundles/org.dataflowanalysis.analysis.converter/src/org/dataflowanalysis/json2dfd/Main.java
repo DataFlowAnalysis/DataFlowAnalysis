c0e0098f60f5d7dfa644075bcfa25bac56559242
uuqjz <nils.niehues@kit.edu>
2024-01-29 10:47:08 +0100

    Create WebDFD nodes, flows and ports from dfd

--- bundles/org.dataflowanalysis.analysis.converter/src/org/dataflowanalysis/json2dfd/Main.java
+++ bundles/org.dataflowanalysis.analysis.converter/src/org/dataflowanalysis/json2dfd/Main.java
@@ -14,53 +14,37 @@ public class Main {
 
 	public static void main(String[] args) {
 		ObjectMapper objectMapper = new ObjectMapper();        
-        File folder = new File("models");
-        
-        // Check if the provided path is a directory
-        if (folder.isDirectory()) {
-            // List all files in the directory
-            File[] files = folder.listFiles();
-
-            // Iterate through each file
-            if (files != null) {
-                for (File file : files) {
-                	List<String> externalEntities = new ArrayList<>();
-                    List<String> services = new ArrayList<>();
-                    List<Flow> flows = new ArrayList<>();
-                    try {
-                        // Parse each file into the SystemConfiguration class
-                        SystemConfiguration systemConfiguration = objectMapper.readValue(file, SystemConfiguration.class);
-
-                        // Process or print information from the parsed object as needed
-                        System.out.println("Parsed JSON from file: " + file.getName());
-                        // Perform additional processing if required
-                        
-                        List<InformationFlow> iflows = systemConfiguration.informationFlows();
-                        for(InformationFlow flow : iflows) {
-                        	flows.add(new Flow(flow.sender(),flow.receiver()));
-                        }
-
-                        for(ExternalEntity ee : systemConfiguration.externalEntities()) {
-                        	externalEntities.add(ee.name());
-                        }
-                        for (Service service:systemConfiguration.services()) {
-                        	services.add(service.name());
-                        }
-                        
-                        new Producer().produce(file.getName().replaceAll("\\.json.*", ""),externalEntities,services,flows);
-
-                    } 
-                    catch (IOException e) {
-                        System.err.println("Error parsing file: " + file.getName());
-                        e.printStackTrace();
-                    }
-                }
-            }
-        } 
-        else {
-            System.err.println("The provided path is not a directory.");
-        }
-        
-	}
+		File file = new File("anilallewar_microservices-basics-spring-boot.json");
+   	                	List<String> externalEntities = new ArrayList<>();
+	                    List<String> services = new ArrayList<>();
+	                    List<Flow> flows = new ArrayList<>();
+	                    try {
+	                        // Parse each file into the SystemConfiguration class
+	                        SystemConfiguration systemConfiguration = objectMapper.readValue(file, SystemConfiguration.class);
 	
-}
+	                        // Process or print information from the parsed object as needed
+	                        System.out.println("Parsed JSON from file: " + file.getName());
+	                        // Perform additional processing if required
+	                        
+	                        List<InformationFlow> iflows = systemConfiguration.informationFlows();
+	                        for(InformationFlow flow : iflows) {
+	                        	flows.add(new Flow(flow.sender(),flow.receiver()));
+	                        }
+	
+	                        for(ExternalEntity ee : systemConfiguration.externalEntities()) {
+	                        	externalEntities.add(ee.name());
+	                        }
+	                        for (Service service:systemConfiguration.services()) {
+	                        	services.add(service.name());
+	                        }
+	                        
+	                        new Producer().produce(file.getName().replaceAll("\\.json.*", ""),externalEntities,services,flows);
+	
+	                    } 
+	                    catch (IOException e) {
+	                        System.err.println("Error parsing file: " + file.getName());
+	                        e.printStackTrace();
+	                    }
+                	}
+    
+            }

7b2eed466e218bbe51718d00f2179128592d2d97
Felix Schwickerath <felix.schwickerath@student.kit.edu>
2024-01-29 21:16:19 +0100

    docs: add comment about issues with the flow graph interface/class

--- bundles/org.dataflowanalysis.analysis/src/org/dataflowanalysis/analysis/core/FlowGraph.java
+++ bundles/org.dataflowanalysis.analysis/src/org/dataflowanalysis/analysis/core/FlowGraph.java
@@ -6,6 +6,7 @@ import java.util.List;
  * This class represents a abstract flow graph that contains all flows contained in a model.
  * The method {@link FlowGraph#findPartialFlowGraphs()} will be called to determine the partial flow graphs for the specific implementation of the flow graph.
  *
+ * TODO: Finding the partial flow graphs needs to happen at the _END_ of the constructors, as {@link FlowGraph#findPartialFlowGraphs()} needs to use attributes from the subclass
  */
 public interface FlowGraph {
 	List<AbstractPartialFlowGraph> findPartialFlowGraphs();

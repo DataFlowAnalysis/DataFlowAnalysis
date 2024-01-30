e66e10e1156d62daf844f0d5724bade625fbf2cb
Felix Schwickerath <felix.schwickerath@student.kit.edu>
2024-01-30 11:42:29 +0100

    docs(pcm): remove comment from propagation logic, as it is the best current solution

--- bundles/org.dataflowanalysis.analysis.pcm/src/org/dataflowanalysis/analysis/pcm/core/PCMPartialFlowGraph.java
+++ bundles/org.dataflowanalysis.analysis.pcm/src/org/dataflowanalysis/analysis/pcm/core/PCMPartialFlowGraph.java
@@ -38,7 +38,6 @@ public class PCMPartialFlowGraph extends AbstractPartialFlowGraph {
 	@Override
     public AbstractPartialFlowGraph evaluate(VertexCharacteristicsCalculator nodeCharacteristicsCalculator, 
     		DataCharacteristicsCalculatorFactory dataCharacteristicsCalculatorFactory) {
-		// TODO: This is incredibly sketchy
         var iterator = this.getVertices().iterator();
         Deque<List<DataFlowVariable>> variableContexts = new ArrayDeque<>();
         variableContexts.push(new ArrayList<>());

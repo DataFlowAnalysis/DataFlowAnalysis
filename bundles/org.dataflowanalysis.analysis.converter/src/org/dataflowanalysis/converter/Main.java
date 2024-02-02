c35081ea30cc5324ed27ceb41c2005c3daa0f8d9
uuqjz <nils.niehues@kit.edu>
2024-02-02 09:44:18 +0100

    Plant UML to DFD

--- bundles/org.dataflowanalysis.analysis.converter/src/org/dataflowanalysis/converter/Main.java
+++ bundles/org.dataflowanalysis.analysis.converter/src/org/dataflowanalysis/converter/Main.java
@@ -10,12 +10,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Main {
 	public static void readMicro(String path) {
+		readMicro(path,true);
+	}
+	
+	public static void readMicro(String path, boolean direct) {
 		ObjectMapper objectMapper = new ObjectMapper();        
 		File file = new File(path);
         try {
             MicroSecEnd micro = objectMapper.readValue(file, MicroSecEnd.class);
             new ProcessJSON().processMicro(file.getName().replaceAll("\\.json.*", ""),micro);
-            System.out.println("Micro->DFD: " + file.getName());
+            if(direct) {
+            	System.out.println("Micro->DFD: " + file.getName());
+            } 
         } 
         catch (IOException e) {
             System.err.println("Error parsing file: " + file.getName());
@@ -41,6 +47,25 @@ public class Main {
 		new ProcessDFD().parse(name+".dataflowdiagram", name+".datadictionary", outfile);
 		System.out.println("DFD->Web: " + name);
 	}
+	
+	public static void readPlant(String path) {
+		String name = path.split("\\.")[0];
+		try {
+            String[] command = {"python3", "convert_model.py", path , "json", "-op", name+".json"};
+
+            ProcessBuilder processBuilder = new ProcessBuilder(command);
+            Process process = processBuilder.start();
+
+            int exitCode = process.waitFor();
+            if(exitCode == 0) {
+            	readMicro(name+".json",false);
+            	System.out.println("Plant->DFD: " + path);
+            }
+        } catch (IOException | InterruptedException e) {
+        	System.out.println("Error converting Plant to JSON");
+            e.printStackTrace();
+        }
+	}
 
 	public static void main(String[] args) {
 		readMicro("anilallewar_microservices-basics-spring-boot.json");
@@ -49,5 +74,7 @@ public class Main {
         readDFD("minimal", "test2.json");
         readWeb("fullweb.json");
         readDFD("fullweb","test3.json");
+        readPlant("9.txt");
+        readDFD("9","test4.json");
 	} 
 }

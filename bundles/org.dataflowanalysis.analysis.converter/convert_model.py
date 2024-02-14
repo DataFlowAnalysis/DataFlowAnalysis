7bf82fc8761c052fb9eb2c0da1418c8cb3e0d3e7
uuqjz <nils.niehues@kit.edu>
2024-02-14 14:21:35 +0100

    Added header to python script

--- bundles/org.dataflowanalysis.analysis.converter/convert_model.py
+++ bundles/org.dataflowanalysis.analysis.converter/convert_model.py
@@ -1,3 +1,7 @@
+# Script written by TUHH, sourced from their GitHub repository:
+# https://github.com/tuhh-softsec/microSecEnD
+# Licensed under the Apache License, Version 2.0:
+# http://www.apache.org/licenses/LICENSE-2.0
 import argparse
 import ast
 import json

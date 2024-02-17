2c6820233e1ec6d6ceafdef62101481cecbc66de
uuqjz <nils.niehues@kit.edu>
2024-02-17 21:44:12 +0100

    Made TVD less fishy

--- bundles/org.dataflowanalysis.analysis.converter/src/org/dataflowanalysis/analysis/converter/microsecend/TaggedValuesDeserializer.java
+++ bundles/org.dataflowanalysis.analysis.converter/src/org/dataflowanalysis/analysis/converter/microsecend/TaggedValuesDeserializer.java
@@ -7,27 +7,29 @@ import java.util.Map;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 public class TaggedValuesDeserializer extends JsonDeserializer<Map<String, List<String>>> {
 
 	@Override
     public Map<String, List<String>> deserialize(JsonParser parser, DeserializationContext ctxt) throws IOException {
         Map<String, List<String>> result = new HashMap<>();
+        ObjectMapper objectMapper = new ObjectMapper();
 
         while (parser.nextToken() != JsonToken.END_OBJECT) {
             String fieldName = parser.getCurrentName();
-            parser.nextToken(); // Move to the value
+            parser.nextToken();
 
             if (parser.getCurrentToken() == JsonToken.START_ARRAY) {
-                // If it's an array, deserialize as List<String>
-                @SuppressWarnings("unchecked")
-				List<String> values = parser.readValueAs(List.class);
+            	List<String> values = objectMapper.readValue(parser, new TypeReference<List<String>>() {});
                 result.put(fieldName, values);
-            } else {
-                // If it's a single value, wrap it in a list
+            } 
+            else {
                 String singleValue = getValueAsString(parser.readValueAsTree());
                 result.put(fieldName, List.of(singleValue));
             }
@@ -39,8 +41,8 @@ public class TaggedValuesDeserializer extends JsonDeserializer<Map<String, List<
     private String getValueAsString(JsonNode node) {
         if (node.isTextual()) {
             return node.asText();
-        } else {
-            // If it's not a textual node, convert to string
+        } 
+        else {
             return node.toString();
         }
     }

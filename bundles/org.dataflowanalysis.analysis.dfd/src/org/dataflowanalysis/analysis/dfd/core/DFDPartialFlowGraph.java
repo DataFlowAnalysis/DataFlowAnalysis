527705075c911d75769550a3ee8a20a3507a0f14
Felix Schwickerath <felix.schwickerath@student.kit.edu>
2024-02-18 19:15:36 +0100

    refactor(dfd): remove comparable interface from dfd partial flow graph

--- bundles/org.dataflowanalysis.analysis.dfd/src/org/dataflowanalysis/analysis/dfd/core/DFDPartialFlowGraph.java
+++ bundles/org.dataflowanalysis.analysis.dfd/src/org/dataflowanalysis/analysis/dfd/core/DFDPartialFlowGraph.java
@@ -5,16 +5,10 @@ import java.util.HashSet;
 import org.dataflowanalysis.analysis.core.AbstractPartialFlowGraph;
 import org.dataflowanalysis.analysis.core.AbstractVertex;
 
-public class DFDPartialFlowGraph extends AbstractPartialFlowGraph implements Comparable<DFDPartialFlowGraph> {
+public class DFDPartialFlowGraph extends AbstractPartialFlowGraph {
 
     public DFDPartialFlowGraph(AbstractVertex<?> sink) {
         super(sink);
-    }
-
-    @Override
-    public int compareTo(DFDPartialFlowGraph o) {
-        // TODO Auto-generated method stub
-        return 0;
     }
 
     @Override

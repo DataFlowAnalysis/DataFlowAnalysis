acbfbbfcdca2993bd399037b8f601032f062825f
Felix Schwickerath <felix.schwickerath@student.kit.edu>
2024-02-18 16:15:55 +0100

    docs(core): remove old TODO from documentation

--- bundles/org.dataflowanalysis.analysis/src/org/dataflowanalysis/analysis/core/FlowGraph.java
+++ bundles/org.dataflowanalysis.analysis/src/org/dataflowanalysis/analysis/core/FlowGraph.java
@@ -6,8 +6,7 @@ import org.dataflowanalysis.analysis.resource.ResourceProvider;
 /**
  * This class represents a abstract flow graph that contains all flows contained in a model. The method
  * {@link FlowGraph#findPartialFlowGraphs()} will be called to determine the partial flow graphs for the specific
- * implementation of the flow graph. TODO: Finding the partial flow graphs needs to happen at the _END_ of the
- * constructors, as {@link FlowGraph#findPartialFlowGraphs()} needs to use attributes from the subclass
+ * implementation of the flow graph
  */
 public abstract class FlowGraph {
     protected ResourceProvider resourceProvider;

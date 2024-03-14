6efd7f77b0b1fa218ac9c3377f7e42dfd863fc63
Odilo Bügler <udygd@student.kit.edu>
2024-03-14 16:21:09 +0100

    Added IFPCMExtractionStrategyPrefer

---
+++ bundles/org.dataflowanalysis.analysis.pcm.informationflow/src/org/dataflowanalysis/analysis/pcm/informationflow/core/IFPCMExtractionStrategyPrefer.java
@@ -0,0 +1,43 @@
+package org.dataflowanalysis.analysis.pcm.informationflow.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.dataflowanalysis.analysis.resource.ResourceProvider;
+import org.dataflowanalysis.pcm.extension.model.confidentiality.ConfidentialityVariableCharacterisation;
+import org.dataflowanalysis.pcm.extension.model.confidentiality.expression.LhsEnumCharacteristicReference;
+
+public class IFPCMExtractionStrategyPrefer extends IFPCMExtractionStrategy {
+
+	public IFPCMExtractionStrategyPrefer(ResourceProvider resourceProvider) {
+		super(resourceProvider);
+	}
+
+	@Override
+	protected List<ConfidentialityVariableCharacterisation> calculateResultingConfidentialityVaraibleCharacterisations(
+			List<ConfidentialityVariableCharacterisation> calculatedCharacterisations,
+			List<ConfidentialityVariableCharacterisation> definedCharacterisations) {
+
+		List<ConfidentialityVariableCharacterisation> resultingCharacterisations = new ArrayList<>(
+				definedCharacterisations);
+		for (ConfidentialityVariableCharacterisation calculatedCharacterisation : calculatedCharacterisations) {
+			LhsEnumCharacteristicReference lhsCalcChar = (LhsEnumCharacteristicReference) calculatedCharacterisation
+					.getLhs();
+			boolean isDefined = false;
+			for (ConfidentialityVariableCharacterisation definedCharacterisation : definedCharacterisations) {
+				LhsEnumCharacteristicReference lhsDefChar = (LhsEnumCharacteristicReference) definedCharacterisation
+						.getLhs();
+				if (lhsCalcChar.getLiteral().getName().equals(lhsDefChar.getLiteral().getName()) && lhsCalcChar
+						.getCharacteristicType().getName().equals(lhsDefChar.getCharacteristicType().getName())) {
+					isDefined = true;
+				}
+			}
+			if (!isDefined) {
+				resultingCharacterisations.add(calculatedCharacterisation);
+			}
+		}
+
+		return resultingCharacterisations;
+	}
+
+}

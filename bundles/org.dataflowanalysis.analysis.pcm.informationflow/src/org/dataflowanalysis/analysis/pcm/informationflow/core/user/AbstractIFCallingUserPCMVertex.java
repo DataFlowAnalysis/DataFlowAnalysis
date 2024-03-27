128a83931e040c8f23630eafca9e59cb7acefc81
Odilo Bügler <udygd@student.kit.edu>
2024-03-27 18:04:18 +0100

    Implemented vertices for calling and returning user behavior

--- bundles/org.dataflowanalysis.analysis.pcm.informationflow/src/org/dataflowanalysis/analysis/pcm/informationflow/core/user/AbstractIFCallingUserPCMVertex.java
+++ bundles/org.dataflowanalysis.analysis.pcm.informationflow/src/org/dataflowanalysis/analysis/pcm/informationflow/core/user/AbstractIFCallingUserPCMVertex.java
@@ -1,16 +1,21 @@
 package org.dataflowanalysis.analysis.pcm.informationflow.core.user;
 
 import java.util.List;
+import java.util.Map;
 
+import org.dataflowanalysis.analysis.core.DataFlowVariable;
 import org.dataflowanalysis.analysis.pcm.core.AbstractPCMVertex;
 import org.dataflowanalysis.analysis.pcm.core.user.CallingUserPCMVertex;
 import org.dataflowanalysis.analysis.pcm.informationflow.core.IFConfigurablePCMVertex;
 import org.dataflowanalysis.analysis.pcm.informationflow.core.IFPCMExtractionStrategy;
+import org.dataflowanalysis.analysis.pcm.informationflow.core.IFSecurityContextUtils;
 import org.dataflowanalysis.analysis.resource.ResourceProvider;
+import org.dataflowanalysis.pcm.extension.model.confidentiality.ConfidentialityVariableCharacterisation;
+import org.palladiosimulator.pcm.parameter.VariableCharacterisation;
 import org.palladiosimulator.pcm.usagemodel.EntryLevelSystemCall;
 
 public abstract class AbstractIFCallingUserPCMVertex extends CallingUserPCMVertex implements IFConfigurablePCMVertex {
-	
+
 	private boolean considerImplicitFlow;
 	private IFPCMExtractionStrategy extractionStrategy;
 
@@ -20,7 +25,7 @@ public abstract class AbstractIFCallingUserPCMVertex extends CallingUserPCMVerte
 		super(element, previousElements, isCalling, resourceProvider);
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	public void setConsiderImplicitFlow(boolean consider) {
 		this.considerImplicitFlow = consider;
 	}
@@ -36,5 +41,54 @@ public abstract class AbstractIFCallingUserPCMVertex extends CallingUserPCMVerte
 	public IFPCMExtractionStrategy getExtractionStrategy() {
 		return extractionStrategy;
 	}
+
+	/*
+	 * Same as in AbstractIFSEFFPCMVertex:
+	 */
+	// TODO Less redundant way?
+
+	@Override
+	public AbstractPCMVertex<?> deepCopy(Map<AbstractPCMVertex<?>, AbstractPCMVertex<?>> isomorphism) {
+		if (isomorphism.get(this) != null) {
+			return isomorphism.get(this);
+		}
+		AbstractIFCallingUserPCMVertex copy = createIFUserVertex(getReferencedElement(), List.of(), resourceProvider);
+		copy.setConsiderImplicitFlow(isConsideringImplicitFlow());
+		copy.setExtractionStrategy(getExtractionStrategy());
+		return super.updateCopy(copy, isomorphism);
+	}
+
+	protected abstract AbstractIFCallingUserPCMVertex createIFUserVertex(EntryLevelSystemCall element,
+			List<? extends AbstractPCMVertex<?>> previousElements, ResourceProvider resourceProvider);
+
+	@Override
+	public void evaluateDataFlow() {
+		var incomingDataFlowVariables = getIncomingDataFlowVariables();
+		incomingDataFlowVariables = modifyIncomingDataFlowVariables(incomingDataFlowVariables);
+
+		// Security context should only have been added when considering implicit flow
+		var securityContext = IFSecurityContextUtils.getActiveSecurityContext(incomingDataFlowVariables);
+
+		var allVariableCharacterisations = extractVariableCharacterisations();
+		var effectiveVariableCharacterisations = getExtractionStrategy()
+				.calculateEffectiveConfidentialityVariableCharacterisation(allVariableCharacterisations,
+						securityContext);
+		checkConfidentialityVariableCharacterisations(effectiveVariableCharacterisations);
+
+		var outgoingDataFlowVariables = getDataFlowVariables(getVertexCharacteristics(),
+				effectiveVariableCharacterisations, incomingDataFlowVariables);
+		outgoingDataFlowVariables = modifyOutgoingDataFlowVariables(outgoingDataFlowVariables);
+
+		setPropagationResult(incomingDataFlowVariables, outgoingDataFlowVariables, getVertexCharacteristics());
+	}
+
+	protected abstract List<DataFlowVariable> modifyIncomingDataFlowVariables(List<DataFlowVariable> incomingVariables);
+
+	protected abstract List<VariableCharacterisation> extractVariableCharacterisations();
+
+	protected abstract void checkConfidentialityVariableCharacterisations(
+			List<ConfidentialityVariableCharacterisation> characterisations);
+
+	protected abstract List<DataFlowVariable> modifyOutgoingDataFlowVariables(List<DataFlowVariable> outgoingVariables);
 
 }
--- bundles/org.dataflowanalysis.analysis.pcm.informationflow/src/org/dataflowanalysis/analysis/pcm/informationflow/core/user/IFCallingUserPCMVertex.java
+++ bundles/org.dataflowanalysis.analysis.pcm.informationflow/src/org/dataflowanalysis/analysis/pcm/informationflow/core/user/IFCallingUserPCMVertex.java
@@ -2,8 +2,11 @@ package org.dataflowanalysis.analysis.pcm.informationflow.core.user;
 
 import java.util.List;
 
+import org.dataflowanalysis.analysis.core.DataFlowVariable;
 import org.dataflowanalysis.analysis.pcm.core.AbstractPCMVertex;
 import org.dataflowanalysis.analysis.resource.ResourceProvider;
+import org.dataflowanalysis.pcm.extension.model.confidentiality.ConfidentialityVariableCharacterisation;
+import org.palladiosimulator.pcm.parameter.VariableCharacterisation;
 import org.palladiosimulator.pcm.usagemodel.EntryLevelSystemCall;
 
 public class IFCallingUserPCMVertex extends AbstractIFCallingUserPCMVertex {
@@ -12,6 +15,34 @@ public class IFCallingUserPCMVertex extends AbstractIFCallingUserPCMVertex {
 			ResourceProvider resourceProvider) {
 		super(element, previousElements, true, resourceProvider);
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected AbstractIFCallingUserPCMVertex createIFUserVertex(EntryLevelSystemCall element,
+			List<? extends AbstractPCMVertex<?>> previousElements, ResourceProvider resourceProvider) {
+		return new IFCallingUserPCMVertex(element, previousElements, resourceProvider);
+	}
+
+	@Override
+	protected List<DataFlowVariable> modifyIncomingDataFlowVariables(List<DataFlowVariable> incomingVariables) {
+		return incomingVariables;
+	}
+
+	@Override
+	protected List<VariableCharacterisation> extractVariableCharacterisations() {
+		return getReferencedElement().getInputParameterUsages_EntryLevelSystemCall().stream()
+				.flatMap(it -> it.getVariableCharacterisation_VariableUsage().stream()).toList();
+	}
+
+	@Override
+	protected void checkConfidentialityVariableCharacterisations(
+			List<ConfidentialityVariableCharacterisation> characterisations) {
+		checkCallParameter(getReferencedElement().getOperationSignature__EntryLevelSystemCall(), characterisations);
+	}
+
+	@Override
+	protected List<DataFlowVariable> modifyOutgoingDataFlowVariables(List<DataFlowVariable> outgoingVariables) {
+		return outgoingVariables;
 	}
 
 }

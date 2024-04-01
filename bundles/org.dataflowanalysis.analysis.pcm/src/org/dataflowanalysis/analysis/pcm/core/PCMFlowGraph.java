0c5ba77548eb37d7a2d2506b0e8833106ea69455
Felix Schwickerath <felix@fschwickerath.de>
2024-04-01 18:22:00 +0200

    fix(pcm): allow subclasses of partial flow graphs and vertices

---
+++ bundles/org.dataflowanalysis.analysis.pcm/src/org/dataflowanalysis/analysis/pcm/core/PCMFlowGraph.java
@@ -0,0 +1,31 @@
+package org.dataflowanalysis.analysis.pcm.core;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.apache.log4j.Logger;
+import org.dataflowanalysis.analysis.core.AbstractPartialFlowGraph;
+import org.dataflowanalysis.analysis.core.FlowGraph;
+import org.dataflowanalysis.analysis.pcm.core.finder.PCMPartialFlowGraphFinder;
+import org.dataflowanalysis.analysis.pcm.resource.PCMResourceProvider;
+import org.dataflowanalysis.analysis.resource.ResourceProvider;
+
+public class PCMFlowGraph extends FlowGraph {
+    private static final Logger logger = Logger.getLogger(PCMFlowGraph.class);
+
+    public PCMFlowGraph(PCMResourceProvider resourceProvider) {
+        super(resourceProvider);
+    }
+
+    public PCMFlowGraph(List<? extends AbstractPartialFlowGraph> partialFlowGraphs, ResourceProvider resourceProvider) {
+        super(partialFlowGraphs, resourceProvider);
+    }
+
+    public List<? extends AbstractPartialFlowGraph> findPartialFlowGraphs() {
+        if (!(this.resourceProvider instanceof PCMResourceProvider pcmResourceProvider)) {
+            logger.error("Cannot find partial flow graphs from non-pcm resource provider");
+            throw new IllegalArgumentException("Cannot find partial flow graphs with non-pcm resource provider");
+        }
+        PCMPartialFlowGraphFinder sequenceFinder = new PCMPartialFlowGraphFinder(pcmResourceProvider);
+        return sequenceFinder.findPartialFlowGraphs().parallelStream().map(AbstractPartialFlowGraph.class::cast).collect(Collectors.toList());
+    }
+}

4ad96ceea94530e2571d70531542b92bd340648d
Felix Schwickerath <felix.schwickerath@student.kit.edu>
2024-04-30 11:59:05 +0200

    docs(dsl): remove leftover todo comments

--- bundles/org.dataflowanalysis.analysis/src/org/dataflowanalysis/analysis/dsl/query/QueryDSL.java
+++ bundles/org.dataflowanalysis.analysis/src/org/dataflowanalysis/analysis/dsl/query/QueryDSL.java
@@ -6,13 +6,6 @@ import org.dataflowanalysis.analysis.dsl.AnalysisQuery;
 public class QueryDSL {
     private final AnalysisQuery analysisQuery;
 
-    /*
-    TODO: Missing components:
-    - Support variables instead of strings in selectors
-    - Allow finer control over data characteristics (e.g. variable name, etc.)
-    - Output could generate variable mappings that produced the constraint (e.g. values of the variables causing a violation)
-     */
-
     public QueryDSL() {
         this.analysisQuery = new AnalysisQuery();
     }

0a6e5fd138a39401ffd1b93d7338ca796db4cbd0
Felix Schwickerath <felix@fschwickerath.de>
2024-04-30 11:25:58 +0200

    feat(dsl): finish data classes and dsl classes

--- bundles/org.dataflowanalysis.analysis/src/org/dataflowanalysis/analysis/dsl/ConstraintVariable.java
+++ bundles/org.dataflowanalysis.analysis/src/org/dataflowanalysis/analysis/dsl/ConstraintVariable.java
@@ -2,25 +2,30 @@ package org.dataflowanalysis.analysis.dsl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class ConstraintVariable {
     private final String name;
-    private final List<String> possibleValues;
+    private Optional<List<String>> possibleValues;
 
     public ConstraintVariable(String name, List<String> possibleValues) {
         this.name = name;
-        this.possibleValues = possibleValues;
+        this.possibleValues = Optional.of(possibleValues);
     }
 
     public static ConstraintVariable of(String name) {
         return new ConstraintVariable(name, new ArrayList<>());
     }
 
+    public void setPossibleValues(List<String> possibleValues) {
+        this.possibleValues = Optional.of(possibleValues);
+    }
+
     public String getName() {
         return name;
     }
 
-    public List<String> getPossibleValues() {
+    public Optional<List<String>> getPossibleValues() {
         return possibleValues;
     }
 }

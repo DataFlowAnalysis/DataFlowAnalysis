77d11da562e97237c7fae603b657045acdbd6ebb
Odilo Bügler <udygd@student.kit.edu>
2024-05-10 15:07:33 +0200

    Fixed characterisation of wrong variable in case of modification of specified ConfidentialityVariableCharacterisations for implicit flow in IFConfidentialityVariableCharacterisationUtils

--- bundles/org.dataflowanalysis.analysis.pcm.informationflow/src/org/dataflowanalysis/analysis/pcm/informationflow/core/utils/IFConfidentialityVariableCharacterisationUtils.java
+++ bundles/org.dataflowanalysis.analysis.pcm.informationflow/src/org/dataflowanalysis/analysis/pcm/informationflow/core/utils/IFConfidentialityVariableCharacterisationUtils.java
@@ -204,7 +204,7 @@ public class IFConfidentialityVariableCharacterisationUtils {
             }
         }
 
-        return createCharacterisation(constraint, latticeCharacteristicType, level,
+        return createCharacterisation(characterizedVariable, latticeCharacteristicType, level,
                 createRhsForAdditionalHigherEqualConstraint(lowerDefinedTerms, levelTerm, levelConstraintVariable, higherConstraintVariables));
     }
 

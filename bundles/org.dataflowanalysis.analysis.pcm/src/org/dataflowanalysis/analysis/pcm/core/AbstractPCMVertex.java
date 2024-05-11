37624f7862a892098bda43589ba50fd561cf6309
Felix Schwickerath <felix.schwickerath@student.kit.edu>
2024-05-11 15:01:44 +0200

    fix(pcm): fix hashmap key collisions for pcm elements

--- bundles/org.dataflowanalysis.analysis.pcm/src/org/dataflowanalysis/analysis/pcm/core/AbstractPCMVertex.java
+++ bundles/org.dataflowanalysis.analysis.pcm/src/org/dataflowanalysis/analysis/pcm/core/AbstractPCMVertex.java
@@ -3,6 +3,7 @@ package org.dataflowanalysis.analysis.pcm.core;
 import java.util.Deque;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import org.apache.log4j.Logger;
 import org.dataflowanalysis.analysis.core.AbstractVertex;
@@ -187,5 +188,10 @@ public abstract class AbstractPCMVertex<T extends Entity> extends AbstractVertex
                 .getId()
                 .equals(otherVertex.getReferencedElement()
                         .getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getReferencedElement().getId());
     }
 }

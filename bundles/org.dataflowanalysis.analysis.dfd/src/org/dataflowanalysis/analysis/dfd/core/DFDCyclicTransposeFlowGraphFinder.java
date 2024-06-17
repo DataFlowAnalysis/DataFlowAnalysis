c1bdca668012eda022b586b2f39383e5b3b94c15
BenjaminArp <101420246+BenjaminArp@users.noreply.github.com>
2024-06-17 10:32:16 +0200

    changed implents logic to extends logic
    To make inheritence possible i needed to change private to protected

--- bundles/org.dataflowanalysis.analysis.dfd/src/org/dataflowanalysis/analysis/dfd/core/DFDCyclicTransposeFlowGraphFinder.java
+++ bundles/org.dataflowanalysis.analysis.dfd/src/org/dataflowanalysis/analysis/dfd/core/DFDCyclicTransposeFlowGraphFinder.java
@@ -20,34 +20,22 @@ import org.dataflowanalysis.dfd.dataflowdiagram.Node;
 /**
  * The DFDTransposeFlowGraphFinder determines all transpose flow graphs contained in a model
  */
-public class DFDCyclicTransposeFlowGraphFinder implements TransposeFlowGraphFinder {
-    private final DataDictionary dataDictionary;
-    private final DataFlowDiagram dataFlowDiagram;
+public class DFDCyclicTransposeFlowGraphFinder extends DFDTransposeFlowGraphFinder {
+
 
 
     public DFDCyclicTransposeFlowGraphFinder(DFDResourceProvider resourceProvider) {
-        this.dataDictionary = resourceProvider.getDataDictionary();
-        this.dataFlowDiagram = resourceProvider.getDataFlowDiagram();
+        super(resourceProvider);
     }
 
     public DFDCyclicTransposeFlowGraphFinder(DataDictionary dataDictionary, DataFlowDiagram dataFlowDiagram) {
-        this.dataDictionary = dataDictionary;
-        this.dataFlowDiagram = dataFlowDiagram;
+        super( dataDictionary, dataFlowDiagram);
     }
 
     /**
      * Finds all transpose flow graphs in a dataflowdiagram model instance
      * @return Returns a list of all transpose flow graphs
      */
-    @Override
-    public List<? extends AbstractTransposeFlowGraph> findTransposeFlowGraphs() {
-        return this.findTransposeFlowGraphs(getEndNodes(dataFlowDiagram.getNodes()), List.of());
-    }
-    
-    @Override
-    public List<? extends AbstractTransposeFlowGraph> findTransposeFlowGraphs(List<?> sourceNodes) {
-        return this.findTransposeFlowGraphs(getEndNodes(dataFlowDiagram.getNodes()), sourceNodes);
-    }
 
     @Override
     public List<? extends AbstractTransposeFlowGraph> findTransposeFlowGraphs(List<?> sinkNodes, List<?> sourceNodes) {
@@ -78,15 +66,6 @@ public class DFDCyclicTransposeFlowGraphFinder implements TransposeFlowGraphFind
         return transposeFlowGraphs;
     }
 
-    /**
-     * Builds a list of sink vertices with previous vertices for the creation of transpose flow graphs.
-     * <p/>
-     * This method preforms the determination of sinks recursively
-     * @param sink Single sink vertex without previous vertices calculated
-     * @param flows All flows in the data flow diagram
-     * @param inputPins Relevant input pins on the given vertex vertex
-     * @return List of sinks created from the initial sink with previous vertices calculated
-     */
     /**
      * Builds a list of sink vertices with previous vertices for the creation of transpose flow graphs.
      * <p/>
@@ -160,7 +139,7 @@ public class DFDCyclicTransposeFlowGraphFinder implements TransposeFlowGraphFind
                 previousNodeInputPins, sourceNodes, new ArrayList<>(previousNodesInTransposeFlow));
         
         for (DFDVertex vertex : vertices) {
-            result.addAll(cloneVertexForMultipleFlowGraphs(vertex, inputPin, incomingFlow, previousNodeVertices));
+            result.addAll(cloneFlowandVertexForMultipleFlowGraphs(vertex, inputPin, incomingFlow, previousNodeVertices));
         }
         return result;
     } 
@@ -171,26 +150,6 @@ public class DFDCyclicTransposeFlowGraphFinder implements TransposeFlowGraphFind
                 .count();
         return count <= 2;
     }
-    
-
-    /**
-     * Calculate all input pins required on the previous node that will be needed to satisfy the assignments to reach the
-     * present node
-     * @param previousNode Previous node
-     * @param flow Flow from previous into present node
-     * @return List of all required pins
-     */
-    private List<Pin> getAllPreviousNodeInputPins(Node previousNode, Flow flow) {
-        List<Pin> previousNodeInputPins = new ArrayList<>();
-        for (var assignment : previousNode.getBehaviour()
-                .getAssignment()) {
-            if (assignment.getOutputPin()
-                    .equals(flow.getSourcePin())) {
-                previousNodeInputPins.addAll(assignment.getInputPins());
-            }
-        }
-        return previousNodeInputPins;
-    }
 
     /**
      * Clones a vertex with its predecessors to use in multiple other flow graphs
@@ -200,8 +159,9 @@ public class DFDCyclicTransposeFlowGraphFinder implements TransposeFlowGraphFind
      * @param previousNodeVertices List of previous vertices
      * @return Returns a list of cloned vertices required for usage in multiple flow graphs
      */
-    private List<DFDVertex> cloneVertexForMultipleFlowGraphs(DFDVertex vertex, Pin inputPin, Flow flow, List<DFDVertex> previousNodeVertices) {
+    private List<DFDVertex> cloneFlowandVertexForMultipleFlowGraphs(DFDVertex vertex, Pin inputPin, Flow flow, List<DFDVertex> previousNodeVertices) {
         List<DFDVertex> newVertices = new ArrayList<>();
+        //Flow newFlow = flow.copy()
         for (var previousVertex : previousNodeVertices) {
             DFDVertex newVertex = vertex.copy(new IdentityHashMap<>());
             newVertex.getPinDFDVertexMap()
@@ -211,33 +171,6 @@ public class DFDCyclicTransposeFlowGraphFinder implements TransposeFlowGraphFind
             newVertices.add(newVertex);
         }
         return newVertices;
-    }
-
-    /**
-     * Gets a list of nodes that are sinks of the given list of nodes
-     * @param nodes A list of all nodes of which the sinks should be determined
-     * @return List of sink nodes reachable by the given list of nodes
-     */
-    private List<Node> getEndNodes(List<Node> nodes) {
-        List<Node> endNodes = new ArrayList<>(nodes);
-        for (Node node : nodes) {
-            if (node.getBehaviour()
-                    .getInPin()
-                    .isEmpty())
-                endNodes.remove(node);
-            for (Pin inputPin : node.getBehaviour()
-                    .getInPin()) {
-                for (AbstractAssignment assignment : node.getBehaviour()
-                        .getAssignment()) {
-                    if (assignment.getInputPins()
-                            .contains(inputPin)) {
-                        endNodes.remove(node);
-                        break;
-                    }
-                }
-            }
-        }
-        return endNodes;
     }
 }
 
--- bundles/org.dataflowanalysis.analysis.dfd/src/org/dataflowanalysis/analysis/dfd/core/DFDTransposeFlowGraphFinder.java
+++ bundles/org.dataflowanalysis.analysis.dfd/src/org/dataflowanalysis/analysis/dfd/core/DFDTransposeFlowGraphFinder.java
@@ -17,7 +17,7 @@ import org.dataflowanalysis.dfd.dataflowdiagram.Node;
  */
 public class DFDTransposeFlowGraphFinder implements TransposeFlowGraphFinder {
     private final DataDictionary dataDictionary;
-    private final DataFlowDiagram dataFlowDiagram;
+    protected final DataFlowDiagram dataFlowDiagram;
 
 
     public DFDTransposeFlowGraphFinder(DFDResourceProvider resourceProvider) {
@@ -123,7 +123,7 @@ public class DFDTransposeFlowGraphFinder implements TransposeFlowGraphFinder {
      * @param flow Flow from previous into present node
      * @return List of all required pins
      */
-    private List<Pin> getAllPreviousNodeInputPins(Node previousNode, Flow flow) {
+    protected List<Pin> getAllPreviousNodeInputPins(Node previousNode, Flow flow) {
         List<Pin> previousNodeInputPins = new ArrayList<>();
         for (var assignment : previousNode.getBehaviour()
                 .getAssignment()) {
@@ -143,7 +143,7 @@ public class DFDTransposeFlowGraphFinder implements TransposeFlowGraphFinder {
      * @param previousNodeVertices List of previous vertices
      * @return Returns a list of cloned vertices required for usage in multiple flow graphs
      */
-    private List<DFDVertex> cloneVertexForMultipleFlowGraphs(DFDVertex vertex, Pin inputPin, Flow flow, List<DFDVertex> previousNodeVertices) {
+    protected List<DFDVertex> cloneVertexForMultipleFlowGraphs(DFDVertex vertex, Pin inputPin, Flow flow, List<DFDVertex> previousNodeVertices) {
         List<DFDVertex> newVertices = new ArrayList<>();
         for (var previousVertex : previousNodeVertices) {
             DFDVertex newVertex = vertex.copy(new IdentityHashMap<>());
@@ -161,7 +161,7 @@ public class DFDTransposeFlowGraphFinder implements TransposeFlowGraphFinder {
      * @param nodes A list of all nodes of which the sinks should be determined
      * @return List of sink nodes reachable by the given list of nodes
      */
-    private List<Node> getEndNodes(List<Node> nodes) {
+    protected List<Node> getEndNodes(List<Node> nodes) {
         List<Node> endNodes = new ArrayList<>(nodes);
         for (Node node : nodes) {
             if (node.getBehaviour()

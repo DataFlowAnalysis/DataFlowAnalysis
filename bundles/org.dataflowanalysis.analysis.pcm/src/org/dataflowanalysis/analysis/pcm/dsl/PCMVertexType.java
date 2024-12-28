eee45f4f3c7a5127c747f9bcc4d9737047be6501
Felix Schwickerath <felix.schwickerath@student.kit.edu>
2024-12-28 20:44:50 +0100

    feat(pcm-dsl): implement dsl context provider for pcm

--- bundles/org.dataflowanalysis.analysis.pcm/src/org/dataflowanalysis/analysis/pcm/dsl/PCMVertexType.java
+++ bundles/org.dataflowanalysis.analysis.pcm/src/org/dataflowanalysis/analysis/pcm/dsl/PCMVertexType.java
@@ -1,11 +1,14 @@
 package org.dataflowanalysis.analysis.pcm.dsl;
 
 import org.dataflowanalysis.analysis.core.AbstractVertex;
+import org.dataflowanalysis.analysis.dfd.dsl.DFDVertexType;
 import org.dataflowanalysis.analysis.dsl.selectors.VertexType;
 import org.dataflowanalysis.analysis.pcm.core.AbstractPCMVertex;
 import org.dataflowanalysis.analysis.pcm.core.CallReturnBehavior;
 import org.dataflowanalysis.analysis.pcm.core.seff.SEFFPCMVertex;
 import org.dataflowanalysis.analysis.pcm.core.user.UserPCMVertex;
+import org.dataflowanalysis.analysis.utils.ParseResult;
+import org.dataflowanalysis.analysis.utils.StringView;
 
 public enum PCMVertexType implements VertexType {
     USER,
@@ -35,5 +38,25 @@ public enum PCMVertexType implements VertexType {
                 return false;
             }
         }
+    }
+
+    public static ParseResult<VertexType> fromString(StringView string) {
+        if (string.startsWith("USER")) {
+            string.advance("USER".length());
+            return ParseResult.ok(PCMVertexType.USER);
+        }
+        if (string.startsWith("SEFF")) {
+            string.advance("SEFF".length());
+            return ParseResult.ok(PCMVertexType.SEFF);
+        }
+        if (string.startsWith("CALLING")) {
+            string.advance("CALLING".length());
+            return ParseResult.ok(PCMVertexType.CALLING);
+        }
+        if (string.startsWith("RETURNING")) {
+            string.advance("RETURNING".length());
+            return ParseResult.ok(PCMVertexType.RETURNING);
+        }
+        return ParseResult.error("Invalid pcm vertex type!");
     }
 }

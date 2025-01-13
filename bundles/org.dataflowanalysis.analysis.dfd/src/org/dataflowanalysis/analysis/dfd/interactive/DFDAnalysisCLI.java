eb00181dd730a4d4b6d8662b9736ffa902fcfce1
Felix Schwickerath <felix.schwickerath@student.kit.edu>
2025-01-13 12:05:23 +0100

    feat(cli): allow CLI to specify a path to a dsl file instead of a string

--- bundles/org.dataflowanalysis.analysis.dfd/src/org/dataflowanalysis/analysis/dfd/interactive/DFDAnalysisCLI.java
+++ bundles/org.dataflowanalysis.analysis.dfd/src/org/dataflowanalysis/analysis/dfd/interactive/DFDAnalysisCLI.java
@@ -8,6 +8,10 @@ import org.dataflowanalysis.analysis.dsl.AnalysisConstraint;
 import org.dataflowanalysis.analysis.dsl.result.DSLResult;
 import org.dataflowanalysis.analysis.utils.StringView;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -20,11 +24,11 @@ public class DFDAnalysisCLI {
             System.exit(-1);
         }
         DFDConfidentialityAnalysis analysis;
-        AnalysisConstraint constraint;
+        List<AnalysisConstraint> constraints;
         if (args.length == 0) {
             Scanner scanner = new Scanner(System.in);
             analysis = createAnalysisInteractive(scanner);
-            constraint = createConstraintInteractive(scanner);
+            constraints = createConstraintInteractive(scanner);
             scanner.close();
         } else {
             if (!args[0].endsWith(".dataflowdiagram")) {
@@ -36,15 +40,23 @@ public class DFDAnalysisCLI {
                 System.exit(-1);
             }
             analysis = createAnalysis(args[0], args[1]);
-            constraint = createConstraint(args[2]);
+            if (args[2].endsWith(".dfadsl")) {
+                constraints = createConstraintsFromFile(args[2]);
+            } else {
+                constraints = List.of(createConstraint(args[2]));
+            }
         }
         analysis.initializeAnalysis();
-
         FlowGraphCollection flowGraphs = analysis.findFlowGraphs();
         flowGraphs.evaluate();
-        List<DSLResult> violations = constraint.findViolations(flowGraphs);
-        for (DSLResult violation : violations) {
-            logger.info(violation.toString());
+        for (int i = 0; i < constraints.size(); i++) {
+            AnalysisConstraint constraint = constraints.get(i);
+            List<DSLResult> violations = constraint.findViolations(flowGraphs);
+            for (DSLResult violation : violations) {
+                logger.info("Violation for constraint " + i + ":");
+                logger.info(violation.toString());
+                logger.info("-------------------------");
+            }
         }
         System.exit(0);
     }
@@ -65,10 +77,39 @@ public class DFDAnalysisCLI {
                 .build();
     }
 
-    private static AnalysisConstraint createConstraintInteractive(Scanner scanner) {
-        System.out.print("Please enter a constraint: ");
+
+
+    private static List<AnalysisConstraint> createConstraintInteractive(Scanner scanner) {
+        List<AnalysisConstraint> constraints = new ArrayList<>();
+        System.out.print("Please enter constraints: ");
         String constraintString = scanner.nextLine();
-        return createConstraint(constraintString);
+        while (!constraintString.isEmpty()) {
+            constraints.add(createConstraint(constraintString));
+            System.out.print("Please enter constraints (end with empty line): ");
+            constraintString = scanner.nextLine();
+        }
+        return constraints;
+    }
+
+    private static List<AnalysisConstraint> createConstraintsFromFile(String fileName) {
+        List<AnalysisConstraint> constraints = new ArrayList<>();
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            List<String> lines = bufferedReader.lines().toList();
+            for (int i = 0; i < lines.size(); i++) {
+                var parseResult = AnalysisConstraint.fromString(new StringView(lines.get(i)));
+                if (parseResult.failed()) {
+                    logger.error("Invalid constraint in line" + i + ":");
+                    logger.error(parseResult.getError());
+                    System.exit(-1);
+                }
+                constraints.add(parseResult.getResult());
+            }
+        }
+        catch (IOException e) {
+            logger.error("Could not read file!", e);
+            System.exit(-1);
+        }
+        return constraints;
     }
 
     private static AnalysisConstraint createConstraint(String constraintString) {
--- bundles/org.dataflowanalysis.analysis.pcm/src/org/dataflowanalysis/analysis/pcm/interactive/PCMAnalysisCLI.java
+++ bundles/org.dataflowanalysis.analysis.pcm/src/org/dataflowanalysis/analysis/pcm/interactive/PCMAnalysisCLI.java
@@ -8,6 +8,10 @@ import org.dataflowanalysis.analysis.pcm.PCMDataFlowConfidentialityAnalysis;
 import org.dataflowanalysis.analysis.pcm.PCMDataFlowConfidentialityAnalysisBuilder;
 import org.dataflowanalysis.analysis.utils.StringView;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -16,15 +20,15 @@ public class PCMAnalysisCLI {
 
     public static void main(String[] args) {
         if (args.length != 0 && args.length != 4) {
-            logger.error("Please provide either no arguments, or a path to a .dataflowdiagram and .datadictionary file!");
+            logger.error("Please provide either no arguments, or a path to a .usagemodel, .allocation and .nodecharacteristics file and a constraint!");
             System.exit(-1);
         }
         PCMDataFlowConfidentialityAnalysis analysis;
-        AnalysisConstraint constraint;
+        List<AnalysisConstraint> constraints;
         if (args.length == 0) {
             Scanner scanner = new Scanner(System.in);
             analysis = createAnalysisInteractive(scanner);
-            constraint = createConstraintInteractive(scanner);
+            constraints = createConstraintInteractive(scanner);
             scanner.close();
         } else {
             if (!args[0].endsWith(".usagemodel")) {
@@ -35,16 +39,29 @@ public class PCMAnalysisCLI {
                 logger.error("The second argument should be a path to a .allocation file");
                 System.exit(-1);
             }
+            if (!args[2].endsWith(".nodecharacteristics")) {
+                logger.error("The second argument should be a path to a .nodecharacteristics file");
+                System.exit(-1);
+            }
             analysis = createAnalysis(args[0], args[1], args[2]);
-            constraint = createConstraint(args[3]);
+            if(args[3].endsWith(".dfadsl")) {
+                constraints = createConstraintsFromFile(args[3]);
+            } else {
+                constraints = List.of(createConstraint(args[3]));
+            }
         }
         analysis.initializeAnalysis();
 
         FlowGraphCollection flowGraphs = analysis.findFlowGraphs();
         flowGraphs.evaluate();
-        List<DSLResult> violations = constraint.findViolations(flowGraphs);
-        for (DSLResult violation : violations) {
-            logger.info(violation.toString());
+        for (int i = 0; i < constraints.size(); i++) {
+            AnalysisConstraint constraint = constraints.get(i);
+            List<DSLResult> violations = constraint.findViolations(flowGraphs);
+            for (DSLResult violation : violations) {
+                logger.info("Violation for constraint " + i + ":");
+                logger.info(violation.toString());
+                logger.info("-------------------------");
+            }
         }
         System.exit(0);
     }
@@ -68,10 +85,37 @@ public class PCMAnalysisCLI {
                 .build();
     }
 
-    private static AnalysisConstraint createConstraintInteractive(Scanner scanner) {
-        System.out.print("Please enter a constraint: ");
+    private static List<AnalysisConstraint> createConstraintInteractive(Scanner scanner) {
+        List<AnalysisConstraint> constraints = new ArrayList<>();
+        System.out.print("Please enter constraints: ");
         String constraintString = scanner.nextLine();
-        return createConstraint(constraintString);
+        while (!constraintString.isEmpty()) {
+            constraints.add(createConstraint(constraintString));
+            System.out.print("Please enter constraints (end with empty line): ");
+            constraintString = scanner.nextLine();
+        }
+        return constraints;
+    }
+
+    private static List<AnalysisConstraint> createConstraintsFromFile(String fileName) {
+        List<AnalysisConstraint> constraints = new ArrayList<>();
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            List<String> lines = bufferedReader.lines().toList();
+            for (int i = 0; i < lines.size(); i++) {
+                var parseResult = AnalysisConstraint.fromString(new StringView(lines.get(i)));
+                if (parseResult.failed()) {
+                    logger.error("Invalid constraint in line" + i + ":");
+                    logger.error(parseResult.getError());
+                    System.exit(-1);
+                }
+                constraints.add(parseResult.getResult());
+            }
+        }
+        catch (IOException e) {
+            logger.error("Could not read file!", e);
+            System.exit(-1);
+        }
+        return constraints;
     }
 
     private static AnalysisConstraint createConstraint(String constraintString) {

bcd71909e522e49c17cc81ba00028bd9474fe959
Felix Schwickerath <felix@fschwickerath.de>
2025-03-17 22:47:41 +0100

    style: format after enforcing formatter

--- bundles/org.dataflowanalysis.examplemodels/src/org/dataflowanalysis/examplemodels/TuhhModels.java
+++ bundles/org.dataflowanalysis.examplemodels/src/org/dataflowanalysis/examplemodels/TuhhModels.java
@@ -1,14 +1,13 @@
 package org.dataflowanalysis.examplemodels;
 
+import com.google.common.collect.ImmutableMap;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.google.common.collect.ImmutableMap;
-
 public class TuhhModels {
-    
+
     private static final Map<String, List<Integer>> TUHH_MODELS = ImmutableMap.<String, List<Integer>>builder()
             .put("anilallewar", List.of(0, 6, 7, 8, 9, 11, 12, 18))
             .put("apssouza22", List.of(0, 2, 4, 6, 7, 8, 12, 18))
@@ -26,13 +25,13 @@ public class TuhhModels {
             .put("sqshq", List.of(0, 6, 7, 8, 9, 10, 11, 12, 18))
             .put("yidongnan", List.of(0, 2, 3, 4, 5, 6, 7, 8, 9, 18))
             .build();
-    
+
     /**
      * Returns a deep copy of the {@code TUHH_MODELS} map.
      */
     public static Map<String, List<Integer>> getTuhhModels() {
         Map<String, List<Integer>> deepCopy = new HashMap<>();
-        
+
         for (var entry : TUHH_MODELS.entrySet()) {
             deepCopy.put(entry.getKey(), new ArrayList<>(entry.getValue()));
         }

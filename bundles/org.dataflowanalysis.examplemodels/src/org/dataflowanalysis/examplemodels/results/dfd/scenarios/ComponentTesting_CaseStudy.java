d0fede31f6a3263e9b86cea7b7d93f15e68f9ba1
manuelcorcoles <91339576+manuelcorcoles@users.noreply.github.com>
2025-07-14 16:52:27 +0200

    New scenarios of privacy_paper and industrial cases

---
+++ bundles/org.dataflowanalysis.examplemodels/src/org/dataflowanalysis/examplemodels/results/dfd/scenarios/ComponentTesting_CaseStudy.java
@@ -0,0 +1,46 @@
+package org.dataflowanalysis.examplemodels.results.dfd.scenarios;
+
+import java.util.List;
+import org.dataflowanalysis.analysis.dsl.AnalysisConstraint;
+import org.dataflowanalysis.analysis.dsl.constraint.ConstraintDSL;
+import org.dataflowanalysis.examplemodels.results.ExpectedViolation;
+import org.dataflowanalysis.examplemodels.results.dfd.DFDExampleModelResult;
+
+public class ComponentTesting_CaseStudy implements DFDExampleModelResult {
+
+    @Override
+    public String getBaseFolderName() {
+        return "scenarios";
+    }
+
+    @Override
+    public String getModelName() {
+        return "ComponentTesting_CaseStudy";
+    }
+
+    @Override
+    public List<AnalysisConstraint> getDSLConstraints() {
+        return List.of(new ConstraintDSL().ofData()
+                .withoutLabel("Data", "AccessToken")
+                .withoutLabel("Data", "Request")
+                .neverFlows()
+                .toVertex()
+                .withCharacteristic("Credentials", "Required")
+                .create());
+    }
+
+    @Override
+    public List<ExpectedViolation> getExpectedViolations() {
+        return List.of();
+    }
+
+    @Override
+    public String toString() {
+        return this.getModelName();
+    }
+
+    @Override
+    public String getFileName() {
+        return "diagram";
+    }
+}
---
+++ bundles/org.dataflowanalysis.examplemodels/src/org/dataflowanalysis/examplemodels/results/dfd/scenarios/ECUUpdate_CaseStudy.java
@@ -0,0 +1,47 @@
+package org.dataflowanalysis.examplemodels.results.dfd.scenarios;
+
+import java.util.List;
+import org.dataflowanalysis.analysis.dsl.AnalysisConstraint;
+import org.dataflowanalysis.analysis.dsl.constraint.ConstraintDSL;
+import org.dataflowanalysis.examplemodels.results.ExpectedViolation;
+import org.dataflowanalysis.examplemodels.results.dfd.DFDExampleModelResult;
+
+public class ECUUpdate_CaseStudy implements DFDExampleModelResult {
+
+    @Override
+    public String getBaseFolderName() {
+        return "scenarios";
+    }
+
+    @Override
+    public String getModelName() {
+        return "ECUUpdate_CaseStudy";
+    }
+
+    @Override
+    public List<AnalysisConstraint> getDSLConstraints() {
+        return List.of(new ConstraintDSL().ofData()
+                .withoutLabel("Data", "Token")
+                .withoutLabel("Data", "SW")
+                .withoutLabel("Data", "UpdateInstruction")
+                .neverFlows()
+                .toVertex()
+                .withCharacteristic("Credentials", "Required")
+                .create());
+    }
+
+    @Override
+    public List<ExpectedViolation> getExpectedViolations() {
+        return List.of();
+    }
+
+    @Override
+    public String toString() {
+        return this.getModelName();
+    }
+
+    @Override
+    public String getFileName() {
+        return "diagram";
+    }
+}

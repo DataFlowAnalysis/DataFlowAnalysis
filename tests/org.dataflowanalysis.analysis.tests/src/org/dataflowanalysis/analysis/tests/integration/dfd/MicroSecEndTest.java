873d93dc93f5b696c8fcf19f396e67ca63e2bc7f
Huell <t0st4r@gmail.com>
2025-08-06 12:53:20 +0200

    Fix: GetVertices not listing all vertices when cyclic

--- tests/org.dataflowanalysis.analysis.tests/src/org/dataflowanalysis/analysis/tests/integration/dfd/MicroSecEndTest.java
+++ tests/org.dataflowanalysis.analysis.tests/src/org/dataflowanalysis/analysis/tests/integration/dfd/MicroSecEndTest.java
@@ -163,8 +163,8 @@ public class MicroSecEndTest {
             }
             list.add(innerList);
         }
-        var compareList = List.of(List.of("user", "apache_server", "content_service"),
-                List.of("user", "apache_server", "cart_service", "product_service"), List.of("user", "apache_server", "product_service"));
+        var compareList = List.of(List.of("apache_server", "user", "apache_server", "content_service"),
+                List.of("apache_server", "user", "apache_server", "cart_service", "product_service"), List.of("apache_server", "user", "apache_server", "product_service"));
 
         assertEquals(list, compareList);
     }

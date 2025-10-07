461ad2f2da1a2f208b1ded2dbf64b62eaff70922
Lostlias <woerner.elias@gmail.com>
2025-10-07 12:48:34 +0200

    added two PMS paper DFDs. Also removed PrivateTaxiViolation Test as PT Diagram seems to be faulty and needs to be rebuilt

--- tests/org.dataflowanalysis.analysis.tests/src/org/dataflowanalysis/analysis/tests/integration/ExampleModelsTest.java
+++ tests/org.dataflowanalysis.analysis.tests/src/org/dataflowanalysis/analysis/tests/integration/ExampleModelsTest.java
@@ -68,7 +68,7 @@ public class ExampleModelsTest {
                 Arguments.of(new IFFriendMapNoViolationResult()), Arguments.of(new IFFriendMapViolationResult()),
                 Arguments.of(new IFHospitalNoViolationResult()), Arguments.of(new IFHospitalViolationResult()),
                 Arguments.of(new IFJPMailNoViolationResult()), Arguments.of(new IFJPMailViolationResult()),
-                Arguments.of(new IFPrivateTaxiNoViolationResult()), Arguments.of(new IFPrivateTaxiViolationResult()),
+                Arguments.of(new IFPrivateTaxiNoViolationResult()),
                 Arguments.of(new IFTravelPlannerNoViolationResult()), Arguments.of(new IFTravelPlannerViolationResult()),
                 Arguments.of(new IFWebRTCNoViolationResult()), Arguments.of(new IFWebRTCViolationResult()), Arguments.of(new CWANoViolation()),
                 Arguments.of(new VWCariad()), Arguments.of(new CWAPersonalDataViolation()), Arguments.of(new CWARPIViolation()));

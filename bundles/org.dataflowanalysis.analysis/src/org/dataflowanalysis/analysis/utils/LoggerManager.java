3f9e3da9a7684b019341d534d673f15729c6053c
BenjaminArp <101420246+BenjaminArp@users.noreply.github.com>
2025-12-05 12:23:10 +0100

    Update LoggerManager.java

--- bundles/org.dataflowanalysis.analysis/src/org/dataflowanalysis/analysis/utils/LoggerManager.java
+++ bundles/org.dataflowanalysis.analysis/src/org/dataflowanalysis/analysis/utils/LoggerManager.java
@@ -1,46 +1,57 @@
 package org.dataflowanalysis.analysis.utils;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.EnhancedPatternLayout;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class LoggerManager {
     private static final Level DEFAULT_LOG_LEVEL = Level.INFO;
-
+    
+    private static final AtomicBoolean CONFIGURED = new AtomicBoolean(false);
+    
     private static final LoggerManager instance = new LoggerManager();
 
-    private final List<Logger> loggers;
+    
+    private final HashMap<Class<?>,Logger> loggers;
+    
 
     private LoggerManager() {
-        BasicConfigurator.resetConfiguration();
-        BasicConfigurator.configure(new ANSIConsoleLogger(new EnhancedPatternLayout("%-6r [%p] %-35C{1} - %m%n")));
-        this.loggers = new ArrayList<>();
+        if (CONFIGURED.compareAndSet(false, true)) {
+            org.apache.log4j.LogManager.resetConfiguration();
+            BasicConfigurator.configure(
+                new ANSIConsoleLogger(
+                    new EnhancedPatternLayout("%-6r [%p] %-35C{1} - %m%n")
+                )
+            );
+        }
+        this.loggers = new HashMap<>();
     }
 
     public void setLevel(Level level) {
-        this.loggers.forEach(it -> it.setLevel(level));
+        this.loggers.values().forEach(it -> it.setLevel(level));
     }
 
     public void setLevel(Level level, Class<?> clazz) {
-        Logger logger = this.loggers.stream()
-                .filter(it -> it.getName()
-                        .equals(clazz.getName()))
-                .findAny()
-                .orElseThrow();
+        Logger logger = this.loggers.get(clazz);
         logger.setLevel(level);
     }
 
     public void resetLevel() {
-        this.loggers.forEach(it -> it.setLevel(DEFAULT_LOG_LEVEL));
+        this.loggers.values().forEach(it -> it.setLevel(DEFAULT_LOG_LEVEL));
     }
 
     public static Logger getLogger(Class<?> clazz) {
-        Logger logger = Logger.getLogger(clazz);
-        logger.setLevel(DEFAULT_LOG_LEVEL);
-        instance.loggers.add(logger);
+        Logger logger = instance.loggers.get(clazz);
+        if (logger == null) {
+            logger = Logger.getLogger(clazz);
+            logger.setLevel(DEFAULT_LOG_LEVEL);
+            instance.loggers.put(clazz,logger);
+        }
+        HashMap<Class<?>,Logger> log = instance.loggers;
+        
         return logger;
     }
 

ccf1b9e7e35d4386d43bd457df1d76c92b70d4eb
Lostlias <woerner.elias@gmail.com>
2026-01-12 17:30:38 +0100

    merged main into PurposeLimitationsPaper and rebased

--- tests/org.dataflowanalysis.analysis.tests/src/org/dataflowanalysis/analysis/tests/integration/ExampleModelsTest.java
+++ tests/org.dataflowanalysis.analysis.tests/src/org/dataflowanalysis/analysis/tests/integration/ExampleModelsTest.java
@@ -67,11 +67,10 @@ public class ExampleModelsTest {
 
     private static Stream<Arguments> provideDFDExampleModelViolations() {
         return Stream.of(Arguments.of(new BranchingResult()), Arguments.of(new OnlineShopResult()), Arguments.of(new SimpleOnlineShopResult()),
-<<<<<<< HEAD
                 Arguments.of(new CWANoViolation()), Arguments.of(new VWCariad()), Arguments.of(new CWAPersonalDataViolation()),
                 Arguments.of(new CWARPIViolation()), Arguments.of(new ComponentTestingCaseStudy()), Arguments.of(new ECUUpdateCaseStudy()),
                 Arguments.of(new MiniTwitCaseStudy()), Arguments.of(new HipaaCaseStudy()), Arguments.of(new DocProc()),
-                Arguments.of(new AppleWallet()), Arguments.of(new CMACaseStudy()));
+                Arguments.of(new AppleWallet()), Arguments.of(new CMACaseStudy()),
         		Arguments.of(new ACABACNoViolationResult()), Arguments.of(new ACABACViolationResult()),
         		Arguments.of(new ACContactSMSNoViolationResult()), Arguments.of(new ACContactSMSViolationResult()),
         		Arguments.of(new ACDACNoViolationResult()), Arguments.of(new ACDACViolationResult()),
@@ -82,26 +81,9 @@ public class ExampleModelsTest {
         		Arguments.of(new IFFriendMapNoViolationResult()), Arguments.of(new IFFriendMapViolationResult()), 
         		Arguments.of(new IFHospitalNoViolationResult()), Arguments.of(new IFHospitalViolationResult()),
         		Arguments.of(new IFJPMailNoViolationResult()), Arguments.of(new IFJPMailViolationResult()),
-        		Arguments.of(new IFPrivateTaxiNoViolationResult()), Arguments.of(new IFPrivateTaxiViolationResult()),
+        		Arguments.of(new IFPrivateTaxiNoViolationResult()),
         		Arguments.of(new IFTravelPlannerNoViolationResult()), Arguments.of(new IFTravelPlannerViolationResult()),
         		Arguments.of(new IFWebRTCNoViolationResult()), Arguments.of(new IFWebRTCViolationResult());
-=======
-                Arguments.of(new ACABACNoViolationResult()), Arguments.of(new ACABACViolationResult()),
-                Arguments.of(new ACContactSMSNoViolationResult()), Arguments.of(new ACContactSMSViolationResult()),
-                Arguments.of(new ACDACNoViolationResult()), Arguments.of(new ACDACViolationResult()),
-                Arguments.of(new ACDistanceTrackerNoViolationResult()), Arguments.of(new ACDistanceTrackerViolationResult()),
-                Arguments.of(new ACMACNoViolationResult()), Arguments.of(new ACMACViolationResult()),
-                Arguments.of(new ACTravelPlannerNoViolationResult()), Arguments.of(new ACTravelPlannerViolationResult()),
-                Arguments.of(new IFContactSMSNoViolationResult()), Arguments.of(new IFContactSMSViolationResult()),
-                Arguments.of(new IFDistanceTrackerNoViolationResult()), Arguments.of(new IFDistanceTrackerViolationResult()),
-                Arguments.of(new IFFriendMapNoViolationResult()), Arguments.of(new IFFriendMapViolationResult()),
-                Arguments.of(new IFHospitalNoViolationResult()), Arguments.of(new IFHospitalViolationResult()),
-                Arguments.of(new IFJPMailNoViolationResult()), Arguments.of(new IFJPMailViolationResult()),
-                Arguments.of(new IFPrivateTaxiNoViolationResult()), Arguments.of(new IFTravelPlannerNoViolationResult()),
-                Arguments.of(new IFTravelPlannerViolationResult()), Arguments.of(new IFWebRTCNoViolationResult()),
-                Arguments.of(new IFWebRTCViolationResult()), Arguments.of(new CWANoViolation()), Arguments.of(new VWCariad()),
-                Arguments.of(new CWAPersonalDataViolation()), Arguments.of(new CWARPIViolation()));
->>>>>>> 2d8dceb1 (Fixed)
     }
 
     @ParameterizedTest

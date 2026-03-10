b2cf288bba25dd86a953344f0fdcf56328a31862
Entenwilli <felix.schwickerath@t-online.de>
2026-03-10 12:43:15 +0000

    Deploying to gh-pages from @ DataFlowAnalysis/DataFlowAnalysis@723d1eb397ecfdeda2cf3afd512378d13e20ae95 🚀

---
+++ assets/chunks/channel.DToaGKYk.js
@@ -0,0 +1,1 @@
+import{U as a,C as n}from"../app.DcY1rRFM.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
---
+++ assets/chunks/chunk-AACKK3MU.D2gejEHE.js
@@ -0,0 +1,1 @@
+import{_ as s}from"../app.DcY1rRFM.js";var t,e=(t=class{constructor(i){this.init=i,this.records=this.init()}reset(){this.records=this.init()}},s(t,"ImperativeState"),t);export{e as I};

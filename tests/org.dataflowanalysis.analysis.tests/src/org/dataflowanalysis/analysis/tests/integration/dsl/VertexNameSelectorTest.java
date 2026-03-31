021c299507942356d5044ba16e02ea78fdbcba4f
Felix Schwickerath <felix.schwickerath@student.kit.edu>
2026-03-31 12:16:52 +0200

    test(dsl): expand vertex name selector test for contains operator

--- tests/org.dataflowanalysis.analysis.tests/src/org/dataflowanalysis/analysis/tests/integration/dsl/VertexNameSelectorTest.java
+++ tests/org.dataflowanalysis.analysis.tests/src/org/dataflowanalysis/analysis/tests/integration/dsl/VertexNameSelectorTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.stream.Stream;
+
 import org.dataflowanalysis.analysis.dsl.context.DSLContext;
 import org.dataflowanalysis.analysis.dsl.selectors.VertexNameSelector;
 import org.dataflowanalysis.analysis.utils.ParseResult;
@@ -15,28 +16,32 @@ import org.junit.jupiter.params.provider.MethodSource;
 public class VertexNameSelectorTest {
     @ParameterizedTest
     @MethodSource("correctVertexNameSelectors")
-    public void shouldParseCorrectly(String vertexNameSelectorString, String expectedVertexName) {
-        ParseResult<VertexNameSelector> vertexNameSelector = VertexNameSelector.fromString(new StringView(vertexNameSelectorString),
-                new DSLContext());
+    public void shouldParseCorrectly(String vertexNameSelectorString, String expectedVertexName,
+                                     boolean expectEmpty) {
+        StringView string = new StringView(vertexNameSelectorString);
+        ParseResult<VertexNameSelector> vertexNameSelector = VertexNameSelector.fromString(string, new DSLContext());
         assertTrue(vertexNameSelector.successful());
         assertEquals(expectedVertexName, vertexNameSelector.getResult()
                 .getName());
+        assertEquals(expectEmpty, string.empty());
     }
 
     @ParameterizedTest
     @MethodSource("incorrectVertexNameSelectors")
     public void shouldNotParse(String vertexNameSelectorString) {
-        ParseResult<VertexNameSelector> vertexNameSelector = VertexNameSelector.fromString(new StringView(vertexNameSelectorString),
-                new DSLContext());
+        StringView string = new StringView(vertexNameSelectorString);
+        ParseResult<VertexNameSelector> vertexNameSelector = VertexNameSelector.fromString(string, new DSLContext());
         assertTrue(vertexNameSelector.failed());
+        assertEquals(0, string.getPosition());
     }
 
     private static Stream<Arguments> correctVertexNameSelectors() {
-        return Stream.of(Arguments.of("vertexName name", "name"), Arguments.of("vertexName otherA.otherB", "otherA.otherB"),
-                Arguments.of("vertexName some string with spaces", "some"));
+        return Stream.of(Arguments.of("vertexName name", "name", true), Arguments.of("vertexName otherA.otherB", "otherA.otherB", true),
+                Arguments.of("vertexName some string with spaces", "some", false), Arguments.of("vertexName contains name", "name", true));
     }
 
     private static Stream<Arguments> incorrectVertexNameSelectors() {
-        return Stream.of(Arguments.of("vertexName"), Arguments.of(""), Arguments.of("vertexName "));
+        return Stream.of(Arguments.of("vertexName"), Arguments.of(""), Arguments.of("vertexName "), Arguments.of("vertexName contains"),
+                Arguments.of("vertexName contains "));
     }
 }

a344e952497dbcbb01505b9eadcfcc645546559b
Felix Schwickerath <felix.schwickerath@student.kit.edu>
2026-04-28 14:33:18 +0200

    feat(models): add example model results for kulturpass, pms and smart speaker

---
+++ bundles/org.dataflowanalysis.examplemodels/src/org/dataflowanalysis/examplemodels/results/dfd/scenarios/PatientMonitoringSystemViolationResult.java
@@ -0,0 +1,29 @@
+package org.dataflowanalysis.examplemodels.results.dfd.scenarios;
+
+import java.util.List;
+import org.dataflowanalysis.analysis.dsl.AnalysisConstraint;
+import org.dataflowanalysis.examplemodels.results.ExpectedViolation;
+import org.dataflowanalysis.examplemodels.results.dfd.DFDExampleModelResult;
+
+public class PatientMonitoringSystemViolationResult implements DFDExampleModelResult {
+
+    @Override
+    public String getBaseFolderName() {
+        return "scenarios";
+    }
+
+    @Override
+    public String getModelName() {
+        return "PatientMonitoringSystemPaper";
+    }
+
+    @Override
+    public List<AnalysisConstraint> getDSLConstraints() {
+        return List.of();
+    }
+
+    @Override
+    public List<ExpectedViolation> getExpectedViolations() {
+        return List.of();
+    }
+}
